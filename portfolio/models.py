4f4495316efbb6606d8a318d9a411bad058a86be
Alfonso <allacalle@gmail.com>
2023-09-04 19:22:09 +0200

    Pagina Home

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 
-
 class Project (models.Model):
-    title = models.CharField(_(""), max_length=100)
-    description = models.CharField(_(""), max_length=250)
-    image = models.ImageField(_(""), upload_to='portfolio/images', height_field=None, width_field=None, max_length=None)
-    url = models.URLField(_(""), max_length=200, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField( max_length=250)
+    image = models.ImageField(upload_to='portfolio/images')
+    url = models.URLField(max_length=200, blank=True)
     
